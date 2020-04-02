4344c6947d413db85b6ede05777697068571ee20
mulananda <mulanandaa@gmail.com>
2020-04-03 01:21:58 +0700

    membuat dashboard dan cms untuk admin

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use\App\Models\Transaction;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -18,6 +19,27 @@ class DashboardController extends Controller
     
     public function index()
     {
-        return view ('pages.dashboard');
+        // query untuk penghasilan
+        $income = Transaction::where('transaction_status','SUCCESS')->sum('transaction_total');
+
+        // query untuk penjualan
+        $sales = Transaction::count();
+
+        // query list transaksi
+        $items = Transaction::orderBy('id','DESC')->take(5)->get();
+
+        // query untuk PIE diagram
+        $pie = [
+            'pending' => Transaction::where('transaction_status','PENDING')->count(),
+            'failed' => Transaction::where('transaction_status','FAILED')->count(),
+            'success' => Transaction::where('transaction_status','SUCCESS')->count(),
+        ];
+
+        return view ('pages.dashboard')->with([
+            'income' => $income,
+            'sales' => $sales,
+            'items' => $items,
+            'pie' => $pie
+        ]);
     }
 }
